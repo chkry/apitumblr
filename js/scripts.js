88603c0484e19daf4ed692279bc453916e5fe619
Chakri Reddy <chkryreddy@gmail.com>
2016-04-28 15:34:02 +0530

    Added Tumblr Quote addition code

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,12 @@ $("document").ready(function () {
 
 function getJSONData() {
 
-    var flickrAPI = "http://api.tumblr.com/v2/blog/chkry.tumblr.com/posts/photo?callback=?";
+    var tumblrAPI = "http://api.tumblr.com/v2/blog/chkry.tumblr.com/posts/photo?callback=?";
 
-    $.getJSON(flickrAPI, {
+    $.getJSON(tumblrAPI, {
         api_key: 'ivofJBCOeNzCg3quY2l7PxnMm0w1bCQb7e3X84erImRG9stL1X'
         , tags: "chkry",
-        limit:30
+        limit:10
     }, successFn);
 
 }
@@ -26,6 +26,57 @@ function successFn(result) {
                     $("<img>").attr("src",cdn).appendTo("#tumblr");
 //                console.log(item1.original_size.url);
                 });
+//                if (i === 50) {
+//                        return false;
+//                    }
+            });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// For QUotes
+
+$("document").ready(function () {
+    getJSONDataQuote();
+});
+
+function getJSONDataQuote() {
+
+    var tumblrAPI = "https://api.tumblr.com/v2/blog/chkry.tumblr.com/posts/quote?callback=?";
+
+    $.getJSON(tumblrAPI, {
+        api_key: 'ivofJBCOeNzCg3quY2l7PxnMm0w1bCQb7e3X84erImRG9stL1X',
+        type:"quote",
+        limit:1
+    }, successFn1);
+
+}
+
+
+
+function successFn1(result) {
+    console.log("Success function initated");
+    console.log(result);
+    $.each(result.response.posts, function (i, item) {
+//            console.log(result.response.posts);
+//            $.each(item.text, function (j, item1) {
+                    var quote = item.text;
+                    console.log(quote);
+//                    $("<img>").attr("src",cdn).appendTo("#tumblr");
+        $("#tumblrQuote").html(quote);
+//                console.log(item1.original_size.url);
+//                });
 //                if (i === 50) {
 //                        return false;
 //                    }
